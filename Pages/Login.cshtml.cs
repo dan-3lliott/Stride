c7a6ee76c9e93a0dd234e772db42477247bc655d
dan-3lliott <elliott.daniel.ballantyne@gmail.com>
2019-12-17 16:24:20 -0700

    Added Login Screen

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stride.Pages
+{
+    public class Login : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
