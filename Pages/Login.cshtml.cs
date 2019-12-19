da6be3136c24ff376dc02f7c31d349f9a06e7294
dan-3lliott <elliott.daniel.ballantyne@gmail.com>
2019-12-18 21:08:29 -0700

    Working on Login

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -4,8 +4,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Stride.Pages
 {
+    [IgnoreAntiforgeryToken(Order = 1001)]
     public class Login : PageModel
     {
-        
+        public void OnPost(string user, string pass)
+        {
+            //do something
+        }
     }
 }
