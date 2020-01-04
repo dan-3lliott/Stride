a70849e36af061620711a280d2dda990485be865
dan-3lliott <elliott.daniel.ballantyne@gmail.com>
2020-01-03 18:39:38 -0700

    Added New SQL

--- Sql/stride.sql
+++ Sql/stride.sql
@@ -11,3 +11,16 @@ GRANT CREATE, INSERT, SELECT, UPDATE ON Stride TO 'dan'@'localhost';
 INSERT INTO Users VALUES ('dan', 'danpassword');
 
 SELECT * FROM users WHERE username = 'dan' and password = 'danpassword';
+
+CREATE TABLE Students (
+    studentnumber int,
+    eduplan varchar(255),
+    college varchar(255),
+    careerpath varchar(255),
+    ethnicity varchar(255),
+    gender varchar(255)
+);
+
+INSERT INTO Students VALUES ('1234567', 'Advanced Degree', 'UCLA', 'Aerospace Engineering', 'White or Caucasian', 'Female')
+
+UPDATE students SET eduplan = 'Bachelor''s Degree' WHERE studentnumber = '1234567';
