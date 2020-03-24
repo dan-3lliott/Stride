390828fc40bf52cc9fc007c793969bfaf607bad8
dan-3lliott <elliott.daniel.ballantyne@gmail.com>
2020-03-24 00:13:51 -0600

    Added search algorithm

--- wwwroot/js/table.js
+++ wwwroot/js/table.js
@@ -16,9 +16,40 @@ function emailSelected() {
     }
 }
 
+function selectAll() {
+    let tr = document.getElementById("studenttable").getElementsByTagName("tr");
+    for (let i = 1; i < tr.length; i++) {
+        if (tr[i].style.display !== "none") {
+            tr[i].classList.add('selected');
+        }
+    }
+}
 function deselect() {
     $("tr").each(function() {
         $(this).removeClass('selected');
     });
 }
 
+function search() {
+    let input, filter, table, tr, td, i, txtValue, searchby;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("studenttable");
+    tr = table.getElementsByTagName("tr");
+    searchby = document.getElementById("searchby");
+    let matchingrows = [];
+    for (i = 1; i < tr.length; i++) {
+        tr[i].classList.remove("last-row");
+        td = tr[i].getElementsByTagName("td")[searchby.options[searchby.selectedIndex].value];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                matchingrows.push(i);
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+    tr[matchingrows[matchingrows.length - 1]].classList.add("last-row");
+}
